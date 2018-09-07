0847104b87baf715e5485d920e7ea7b9e9f1a4a1
SuyashMore <suyash.21.more@gmail.com>
2018-09-07 13:56:25 +0530

    Added Debug Messages

--- scripts/AtmegaNode/NavigationMaster.cpp
+++ scripts/AtmegaNode/NavigationMaster.cpp
@@ -10,6 +10,7 @@ void nav_Complete_1_Row(botData& newSensor,botData& oldSensor,Motor& motor)
 {
 	if(state.currentStepIndex == 1)		//Navigate to Main Line 
 	{
+		cout<<"Navigation Step:"<<1<<endl;
 		if(nav_2_MainLine(newSensor,oldSensor,motor))
 		{
 			state.currentStepIndex++;
@@ -17,6 +18,7 @@ void nav_Complete_1_Row(botData& newSensor,botData& oldSensor,Motor& motor)
 	}
 	else if(state.currentStepIndex == 2)	//Go Forward One Junction
 	{
+		cout<<"Navigation Step:"<<2<<endl;
 		if(nav_goForward_1_Junction(newSensor,oldSensor,motor))
 		{
 			state.currentStepIndex++;
@@ -24,6 +26,7 @@ void nav_Complete_1_Row(botData& newSensor,botData& oldSensor,Motor& motor)
 	}
 	else if( state.currentStepIndex ==3 )		//Go Forward One Junction
 	{
+		cout<<"Navigation Step:"<<3<<endl;
 		if(nav_goForward_1_Junction(newSensor,oldSensor,motor))
 		{
 			state.currentStepIndex++;
@@ -31,6 +34,7 @@ void nav_Complete_1_Row(botData& newSensor,botData& oldSensor,Motor& motor)
 	}
 	else if(state.currentStepIndex==2)	//Rotate and the Pickup the Block from Supply Line and then Return Back to the Same
 	{
+		cout<<"Navigation Step:"<<4<<endl;
 		if(nav_PickupBlock_from__SupplyLine(newSensor,oldSensor,motor))
 		{
 			state.currentStepIndex++;
@@ -38,6 +42,7 @@ void nav_Complete_1_Row(botData& newSensor,botData& oldSensor,Motor& motor)
 	}
 	else if(state.currentStepIndex==3)		//Stack the Block
 	{
+		cout<<"Navigation Step:"<<5<<endl;
 		if(stack_the_Block_from_MainJunction_at_hx(150,newSensor,oldSensor,motor))
 		{
 			state.currentStepIndex++;
@@ -46,6 +51,7 @@ void nav_Complete_1_Row(botData& newSensor,botData& oldSensor,Motor& motor)
 	}
 	else if(state.currentStepIndex==4)
 	{
+		cout<<"Navigation Step:"<<6<<endl;
 		if(nav_goBackward_1_Junction(newSensor,oldSensor,motor))
 		{
 			state.currentStepIndex++;

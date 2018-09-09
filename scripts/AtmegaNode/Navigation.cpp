199abf8b6a7fe8c171e6dfb3b8d98cd528abd879
SuyashMore <suyash.21.more@gmail.com>
2018-09-09 14:43:21 +0530

    Trail :Side Balance 12

--- scripts/AtmegaNode/Navigation.cpp
+++ scripts/AtmegaNode/Navigation.cpp
@@ -460,14 +460,14 @@ bool stack_the_Block_from_MainJunction_at_hx(float targetDistance,botData& newSe
         {
             cout<<"Currently Executing: Going Forward Till TOF:Forward"<<endl;
 			// processPID(newSensor,oldSensor,motor);
-            K_processPID(newSensor,oldSensor,motor,105,80,0.11);
+            K_processPID(newSensor,oldSensor,motor,100,70,0.11);
 			motor.bot_Forward_withPWMm(140);
             q=0;   
         }
         else if(newSensor.tofFront<(TOF_FRONT_BALANCE_DISTANCE-TOF_ERROR_THRESH))
         {
             cout<<"Currently Executing: Going Forward Till TOF:Backward"<<endl;
-            K_processPID(newSensor,oldSensor,motor,105,80,0.11);
+            K_processPID(newSensor,oldSensor,motor,100,70,0.11);
             motor.bot_Backward_withPWMm(140);   
             q=0;
         }

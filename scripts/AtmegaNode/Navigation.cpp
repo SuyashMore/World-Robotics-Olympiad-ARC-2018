b9f91f40bcdffa109754d7ffb9fd868460eebe7f
SuyashMore <suyash.21.more@gmail.com>
2018-09-09 19:50:45 +0530

    Trial:Balance and Drop and Push 5

--- scripts/AtmegaNode/Navigation.cpp
+++ scripts/AtmegaNode/Navigation.cpp
@@ -523,11 +523,11 @@ bool stack_the_Block_from_MainJunction_at_hx(float targetDistance,botData& newSe
          cout<<"Currently Executing: Correcting with Side"<<endl;
           if(newSensor.tofSide > targetDistance)
           {
-              motor.strafe_Left_withPWM(80);
+              motor.strafe_Left_withPWM(150);
           }
           else if(newSensor.tofSide < targetDistance)
           {
-              motor.strafe_Right_withPWM(80);
+              motor.strafe_Right_withPWM(150);
           }
           if(abs(newSensor.tofSide - targetDistance) <=8)
           {

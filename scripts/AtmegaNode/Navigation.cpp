ebce20f4d18cc7b42434f4e748266b6f7c9e7d7d
SuyashMore <suyash.21.more@gmail.com>
2018-09-10 22:20:33 +0530

    Aligning with Wall 23

--- scripts/AtmegaNode/Navigation.cpp
+++ scripts/AtmegaNode/Navigation.cpp
@@ -380,7 +380,6 @@ bool stack_the_Block_from_MainJunction_at_hx(float targetDistance,botData& newSe
               {
                 q=0;
                 cout<<"Modifying Front PWM"<<endl;
-                fmodx = 500;
                 motor.strafe_Left_withPWM(100);
               }
               else

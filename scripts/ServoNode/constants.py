9ce752cdc1b2dc594cd8a373b5b8d866768aa466
ajaykumar pal <palajay1023@gmail.com>
2018-09-13 18:45:42 +0530

    TOF Balance2

--- scripts/ServoNode/constants.py
+++ scripts/ServoNode/constants.py
@@ -2,11 +2,11 @@
 # ttyUSB_USB2DYNAMIXEL = "/dev/ttyUSB9"
 # ttyUSB_USB2DYNAMIXEL = "/dev/ttyUSB6"
 # ttyUSB_USB2DYNAMIXEL = "/dev/ttyUSB0"
-ttyUSB_USB2DYNAMIXEL = "/dev/ttyUSB1"
+ttyUSB_USB2DYNAMIXEL = "/dev/ttyUSB0"
 
-DXL_LIB_PATH = "/home/ajaykumar/DynamixelSDK/c/build/linux64/libdxl_x64_c.so"
+# DXL_LIB_PATH = "/home/ajaykumar/DynamixelSDK/c/build/linux64/libdxl_x64_c.so"
 # DXL_LIB_PATH = "/home/harsh/DynamixelSDK/c/build/linux64/libdxl_x64_c.so"
-# DXL_LIB_PATH = "/home/nvidia/DynamixelSDK/c/build/linux_sbc/libdxl_sbc_c.so"
+DXL_LIB_PATH = "/home/nvidia/DynamixelSDK/c/build/linux_sbc/libdxl_sbc_c.so"
 # DXL_LIB_PATH = "/home/pratik/DynamixelSDK/c/build/linux64/libdxl_x64_c.so"
 
 ENABLE_DXL_MESSAGES = False

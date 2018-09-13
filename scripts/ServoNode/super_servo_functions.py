f24d24bd1d1c2e049d38ec3a399661c5579bdbcd
ajaykumar pal <palajay1023@gmail.com>
2018-09-13 19:29:45 +0530

    fixing navi 3

--- scripts/ServoNode/super_servo_functions.py
+++ scripts/ServoNode/super_servo_functions.py
@@ -307,7 +307,7 @@ def move2standard():
    # move2angle(0,-90,90,0,0)   
     # move2angle(0,-90,90,0,0)
     FINAL_X =11
-    FINAL_Y = -9.3
+    FINAL_Y = -9.5
     INITIAL_Z = -6
     move2angle(-36,-31,112,-82,30)
     setTransformedAngle(4,29)

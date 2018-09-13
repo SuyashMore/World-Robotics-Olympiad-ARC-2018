12bdde7a363e8c030e1b42e66e5f38f7efdc1865
ajaykumar pal <palajay1023@gmail.com>
2018-09-13 18:49:25 +0530

    TOF Balance3

--- scripts/AtmegaNode/utility.h
+++ scripts/AtmegaNode/utility.h
@@ -13,7 +13,7 @@ double Vmap(double input,double inLow,double inHigh,double outLow,double outHigh
 }
 
 bool navFlag = true;
-bool camFlag=true;
+bool camFlag=false;
 bool stopFlag = false;
 int stopFlagIterations = 0;
 int maxStopFlagItr = 70;
